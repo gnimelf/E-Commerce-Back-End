138e0de674493cf71641123ef0d7b7092d17d1e6
justin <fezzygo@gmail.com>
2022-08-05 13:11:24 -0400

    added code to update category based on id (category-route.js:48-59)

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,8 +45,18 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try{
+    const updateCategory = await Category.update(req.body,{
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(req.params.id);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
